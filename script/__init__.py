93b0852c57f2ec49d1796196e259b0a1b7763a3f
Osvaldo Jimenez <osjimene@microsoft.com>
2021-12-17 13:44:43 -0600

    The new init file that will create a powerpoint and return a temporary link for the file.

--- script/__init__.py
+++ script/__init__.py
@@ -1,73 +1,64 @@
+#These are all the requirements in case they dont come up on requirements.txt 
 import logging
-from sys import getprofile
-from pptx import Presentation
-from pptx.util import Inches, Pt
-import argparse
 import pandas as pd
 from datetime import date
-import requests
-from requests.auth import HTTPBasicAuth
-from collections import defaultdict
-from pandas.io.json import json_normalize
-import dateutil.parser
-from pptx.dml.color import RGBColor
-import os
+from script import upload
 from script.ADO_Pull import API_Pull
-from script.Authenticate import getcredentials
-from script.RZ_Selector import RZ_Selector
 from script.create_ppt import create_ppt
-
-from script.putfile import putfile 
-from .GetProfile import userinfo
-from .readDrive import readDrive
+from script.upload import upload_file_to_storage
+from script.RZ_Selector import RZ_Selector
+import os
 
 import azure.functions as func
 
 
-
+#Main entry function to call the HTTP request.
 def main(req: func.HttpRequest) -> func.HttpResponse:
+    blob_sas_url = ""
+    message = ""
+    http_status = 200
+    
     logging.info('Python HTTP trigger function processed a request.')
     #List makes sure that the only Parameters accepted by the API are the ones below.     
     RZList = ['M365','Azure','EMM','Identity','Power','PBI','SDP','MIP','D365','Commerce']
     #Checks for the declared paremeter in the Function URL e.g. Localhost.api/script?RedZone=MIP
     RedZone = req.params.get('RedZone')
-    User = req.params.get('User')
-    #Just a test function to make sure I know where the onedrive/AAD information is pulling from and storing into. 
-    if User == 'Info':
-        graphkey = getcredentials() 
-        response = readDrive(graphkey)
-        return func.HttpResponse(response)
     #Checks the body of the API request in case the value is sent through the body via JSON
     if not RedZone:
         try: 
+            logging.info("Attempting to get parameters via the API Request body...")
             req_body = req.get_json()
         except ValueError:
             pass
         else:
+            logging.info("Attempting to parse out the RedZone from the json body...")
             RedZone = req_body.get('RedZone')
+            logging.info(f"successfully grabbed the body request [{RedZone}] from the request body... ")
     #Chooses the right tags and title for the presentation and the ADO API pull        
+    logging.info('Selecting the proper RedZone variables...')
     meeting = RZ_Selector(RedZone)
-    #If everything runs correctly, you select the proper RedZone to generate for and everything is working then the powerpoint is then pushed to the OneDrive Folder of choice. 
+    #If everything runs correctly, you select the proper RedZone to generate for and everything is working then the powerpoint is then generated. 
     if RedZone in RZList:
         #Sets the Tag and Title variables from the RZ_Selector class
+        logging.info(f"Selected the RZTag[{meeting[0]}] and RZTitle [{meeting[1]} ...]")
         RZTag = meeting[0]
         RZTitle = meeting[1] 
-        #Inputs the proper ADO Tag to then pull all the relevant ADO information via an API call and formats it into a Pandas Dataframe
+        #Function that will pull data from ADO via the API call
+        logging.info("Gathering the ADO Workitem information...")
         data = API_Pull(RZTag)
         #This formats the filename to an appropriate format that can be tracked by date
+        logging.info("Naming file...")
         outfilename = str((RZTitle)+" {:%B %Y}.pptx".format(date.today()))
-        #Creates the powerpoint presentation using the create_ppt class
-        create_ppt(outfilename,data, RZTitle)
-        #Pulls the GraphAPI key from the local.settings.json file
-        #graphkey = os.environ['GraphKey']
-        logging.info('Python HTTP trigger pulled GraphKey from Local.settings.json')
-        #Ammend to use authentication method to grab Graph key
-        graphkey = getcredentials()    
-        #Puts the file information into your OneDrive of choice. 
-        data = putfile(graphkey,outfilename)
-        return func.HttpResponse("File uploaded to your onedrive successfully!")
-    #If not the API call will just tell you that you need to put in a valid entry and all you get is the status code. 
-    else:
-        return func.HttpResponse(meeting, status_code=200)
-        
+        logging.info(f"File named {outfilename} ...")
+        #Function that will create the powerpoint presentation based on the HHTP parameters in the URL
+        presentationfile = create_ppt(outfilename, data, RZTitle)
+        #Function that will upload this generated file into blob storage and then return a temporary URL that the requestor can use. 
+        blob_sas_url = upload_file_to_storage(presentationfile, outfilename)
+        message = " File created and uploaded to storage. You can <a href='" + blob_sas_url + "'>download it </a> for the next 1 hour."
 
+        return func.HttpResponse(
+            mimetype = "text/html",
+            body = message,
+            status_code = http_status
+        )
+        
