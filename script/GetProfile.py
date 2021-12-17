803515d5a3a840ec05801278f63cd3e85c2ff053
Osvaldo Jimenez <osjimene@microsoft.com>
2021-12-17 14:03:48 -0600

    Used to return the UserInfo via the GraphAPI once Authenticated

--- script/GetProfile.py
+++ script/GetProfile.py
@@ -1,5 +1,8 @@
 import requests
 
+#Not used in the main script but some code I used to test the Graph API functionality
+#Used to return the UserInfo via the GraphAPI once Authenticated
+
 def userinfo(secretkey):
     endpoint = 'Https://graph.microsoft.com/v1.0/me'
     secret = 'Bearer ' + secretkey
