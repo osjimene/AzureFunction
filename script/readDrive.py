b987c7fba0e4ce158fbc152e8cb13a58a0274636
Osvaldo Jimenez <osjimene@microsoft.com>
2021-12-17 14:07:54 -0600

    Not used in the function. Used to give information on the targetted OneDrive using the GraphAPI.

--- script/readDrive.py
+++ script/readDrive.py
@@ -1,6 +1,8 @@
 import requests
 import os
 
+#Not used in the function. Used to give information on the targetted OneDrive using the GraphAPI.
+
 def readDrive(secretkey):
     endpoint = 'https://graph.microsoft.com/v1.0/me/drive/root/children'
     secret = 'Bearer '+ secretkey
